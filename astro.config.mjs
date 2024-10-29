75c88d1d4cd48bfc24e62d9e108b6f17b5325f4e
Jonathan Ehwald <github@ehwald.info>
2024-10-29 23:56:25 +0100

    Use header-based redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,26 +7,6 @@ import fs from "fs/promises";
 export default defineConfig({
   site: "https://botfather.io",
   trailingSlash: "always",
-  redirects: {
-    "/docs/legal/": "/imprint/",
-    "/docs/legal/tos/": "/terms/",
-    "/docs/legal/imprint/": "/imprint/",
-    "/docs/legal/privacy/": "/privacy/",
-    "/docs/player/": "/docs/guides/",
-    "/docs/player/what-is-botfather/": "/docs/guides/what-is-botfather/",
-    "/docs/player/cloning-scripts/": "/scripts/",
-    "/docs/wizard/": "/docs/guides/",
-    "/docs/wizard/how-to-create-a-script/":
-      "/docs/guides/how-to-create-a-script/",
-    "/docs/wizard/simple-agario-bot-tutorial/":
-      "/docs/guides/simple-agario-bot-tutorial/",
-    ...Array(59)
-      .keys()
-      .reduce(
-        (acc, val) => ({ ...acc, [`/scripts/${val + 1}/`]: "/scripts/" }),
-        {},
-      ),
-  },
   markdown: {
     remarkPlugins: [[remarkToc, { heading: "Table of contents", maxDepth: 3 }]],
     shikiConfig: {
